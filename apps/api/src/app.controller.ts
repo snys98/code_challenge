fb86d356e7c5c8927f7c9ec1ad7e120d1548b3e4
Lulus <snys98@outlook.com>
2023-12-11 07:35:39 +0800

    add debug profile and redis indicator

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,8 +1,7 @@
-// import Redis from 'ioredis';
-
 import {
   Controller,
   Get,
+  Inject,
 } from '@nestjs/common';
 import {
   HealthCheck,
@@ -10,9 +9,8 @@ import {
   HttpHealthIndicator,
   MongooseHealthIndicator,
 } from '@nestjs/terminus';
-
-// import { InjectRedis } from '@songkeys/nestjs-redis';
-// import { RedisHealthIndicator } from '@songkeys/nestjs-redis-health';
+import { Cache } from 'cache-manager';
+import { RedisHealthIndicator } from '@songkeys/nestjs-redis-health';
 
 const validateStatus = (status) => {
   return status >= 200 && status < 503;
@@ -23,29 +21,38 @@ export class AppController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
-    // @InjectRedis() private readonly redis: Redis,
-    // private redisIndicator: RedisHealthIndicator,
+    @Inject(Cache) private readonly cache: Cache,
+    private redisIndicator: RedisHealthIndicator,
     private mongoose: MongooseHealthIndicator,
-  ) { }
+  ) {
+  }
 
   @Get()
   @HealthCheck()
   check() {
     return this.health.check([
       () => this.mongoose.pingCheck('mongo', { timeout: 1000 }),
-      // () => this.redisIndicator.checkHealth('redis', {
+      () => this.redisIndicator.checkHealth('redis', {
+        timeout: 1000,
+        client: this.cache.store["client"],
+        type: 'redis',
+      }),
+      // () => this.http.pingCheck('apm', 'http://apm-server.dev.challenge', {
+      //   validateStatus,
       //   timeout: 1000,
-      //   client: this.redis,
-      //   type: 'redis',
       // }),
-      () => this.http.pingCheck('apm', 'http://apm-server.dev.challenge', {
-        validateStatus,
-        timeout: 1000,
-      }),
-      () => this.http.pingCheck('elasticsearch', 'http://elasticsearch.dev.challenge', {
-        validateStatus,
-        timeout: 1000,
-      }),
+      // () => this.http.pingCheck('elasticsearch', 'http://localhost:9200', {
+      //   validateStatus,
+      //   timeout: 1000,
+      // }),
+      // () => this.http.pingCheck('logstash', 'http://logstash.dev.challenge', {
+      //   validateStatus,
+      //   timeout: 1000,
+      // }),
+      // () => this.http.pingCheck('kibana', 'http://kibana.dev.challenge', {
+      //   validateStatus,
+      //   timeout: 1000,
+      // })
     ]);
   }
 
